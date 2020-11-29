5c116253dba3f9a572dc36b65730eb02b9ea0e60
Ubuntu <datonearash@gmail.com>
2020-11-28 20:30:31 -0800

    added previous lab

--- source/main.c
+++ source/main.c
@@ -12,6 +12,115 @@
 #include "simAVRHeader.h"
 #endif
 
+enum States {Start, Init, Increment, Decrement, Reset, Wait, WaitA} state;
+unsigned char value;
+
+unsigned char temp;
+unsigned char temp2;
+
+void tick(){
+	temp = ~PINA & 0x01;
+	temp2 = ~PINA & 0x02;
+	switch (state){
+		case Start:
+			state = Init;
+			break;
+		case Init:
+			if(temp && temp2){
+				state = Reset;
+			}
+			else if(temp && !temp2){
+				state = Increment;
+			}
+			else if(temp2 && !temp){
+				state = Decrement;
+			}
+			else{
+				state = Init;
+			}
+			break;
+		case Increment:
+			if(!temp && !temp2){
+				state = Init;
+			}
+			else{
+				state = WaitA;
+			}
+			break;
+		case Decrement:
+			if(temp2 && temp){
+				state = Reset;
+			}
+			else{
+				state = WaitA;
+			}
+			break;
+		case Reset:
+      			if(temp && temp2){
+                                state = Reset;
+                        }
+                        else if(temp && !temp2){
+                                state = Increment;
+                        }
+                        else if(temp2 && !temp){
+                                state = Decrement;
+                        }
+                        else{
+                                state = Init;
+                        }
+                        break;
+		case Wait:
+			if(temp && temp2){
+                                state = Reset;
+                        }
+                        else if(temp && !temp2){
+                                state = Increment;
+                        }
+                        else if(temp2 && !temp){
+                                state = Decrement;
+                        }
+                        else{
+                                state = Wait;
+                        }
+                        break;
+		case WaitA:
+                       if(temp && temp2){
+                                state = Reset;
+                        }
+                        else if(!temp && !temp2){
+                                state = Wait;
+                        }
+                        else{
+                                state = WaitA;
+                        }
+                        break;
+		default:
+			state = Start;
+			break;
+		}
+	switch(state){
+		case Start:
+			break;
+		case Init:
+			break;
+		case Increment:
+			if(value < 9){
+				value++;
+			}
+			break;
+		case Decrement:
+			if(value > 0){
+				value--;
+			}
+			break;
+		case Reset:
+			value = 0x00;
+			break;
+		default:
+			break;
+		}
+}
+
 int main(void) {
     /* Insert DDR and PORT initializations */
 
